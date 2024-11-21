499c8218abfbd1b39f7c0765a292f7d4b6dfdd23
Pau Ferrer <MY_NAME@example.compau-ferrer@minimaldesigns.ie>
2024-11-21 11:00:53 +0100

    Project set up

--- README.md
+++ README.md
@@ -1,36 +1,93 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# Frontend Mentor - Body Mass Index Calculator solution
 
-## Getting Started
+This is a solution to the [Body Mass Index Calculator challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/body-mass-index-calculator-brrBkfSz1T). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
 
-First, run the development server:
+## Table of contents
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Select whether they want to use metric or imperial units
+- Enter their height and weight
+- See their BMI result, with their weight classification and healthy weight range
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./preview.jpg)
+
+### Links
+
+- Solution URL: [Check GitHub repo here](https://your-solution-url.com)
+- Live Site URL: [Check live site here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Next.js](https://nextjs.org/) - React framework
+- [Tailwind CSS](https://tailwindcss.com/) - For styles
+- [Flowbite](https://flowbite.com/) - Components library
+
+### What I learned
+
+Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+
+To see how you can add code snippets, see below:
+
+```html
+<h1>Some HTML code I'm proud of</h1>
+```
+```css
+.proud-of-this-css {
+  color: papayawhip;
+}
+```
+```js
+const proudOfThisFunc = () => {
+  console.log('🎉')
+}
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+**Note: Delete this note and the content within this section and replace with your own learnings.**
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+### Continued development
 
-## Learn More
+Use this section to outline areas that you want to continue focusing on in future projects. These could be concepts you're still not completely comfortable with or techniques you found useful that you want to refine and perfect.
 
-To learn more about Next.js, take a look at the following resources:
+**Note: Delete this note and the content within this section and replace with your own plans for continued development.**
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+### Useful resources
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
+- [Example resource 1](https://www.example.com) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
+- [Example resource 2](https://www.example.com) - This is an amazing article which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept.
 
-## Deploy on Vercel
+**Note: Delete this note and replace the list above with resources that helped you during the challenge. These could come in handy for anyone viewing your solution or for yourself when you look back on this project in the future.**
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+## Author
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+- Website - [Pau Ferrer | MonkeyMinds.io](https://www.monkeyminds.io)
+- Frontend Mentor - [@monkeyminds-io](https://www.frontendmentor.io/profile/monkeyminds-io)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,100 @@
-import Image from "next/image";
+// =============================================================================
+// File Name: Home Page
+// File Description:
+// This file contains the Home Page.
+// =============================================================================
+// =============================================================================
+// Page Imports
+// =============================================================================
+import { Metadata } from 'next'
 
-export default function Home() {
+// =============================================================================
+// Page Props
+// =============================================================================
+
+// =============================================================================
+// Page Metadata
+// =============================================================================
+export const metadata: Metadata = {
+  title: 'Home'
+}
+
+// =============================================================================
+// Page Component
+// =============================================================================
+export const HomePage = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main>
+      Body Mass Index Calculator
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      Better understand your weight in relation to your height using our 
+      body mass index (BM) calculator. While BMI is not the sole determinant 
+      of a healthy weight, it offers a valuable starting point to evaluate 
+      your overall health and well-being.
+
+      Enter your details below
+
+      Metric
+      Imperial
+
+      Height
+      Weight
+
+      Your BMI is...
+
+      {/* add score  */}
+
+      Your BMI suggests you're {/*  add classification */}. 
+      Your ideal weight is between {/*  add range */}.
+
+      What your BMI result means
+
+      A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a 
+      healthy weight may lower your chances of experiencing health issues later on, 
+      such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced 
+      fat and sugar content, incorporating ample fruits and vegetables. Additionally, 
+      strive for regular physical activity, ideally about 30 minutes daily for 
+      five days a week.
+
+      Healthy eating
+      Healthy eating promotes weight control, disease prevention, better digestion, 
+      immunity, mental clarity, and mood.
+
+      Regular exercise
+      Exercise improves fitness, aids weight control, elevates mood, and reduces disease 
+      risk, fostering wellness and longevity.
+
+      Adequate sleep
+      Sleep enhances mental clarity, emotional stability, and physical wellness, promoting 
+      overall restoration and rejuvenation.
+
+      Limitations of BMI
+
+      Although BMI is often a practical indicator of healthy weight, it is not suited for 
+      every person. Specific groups should carefully consider their BMI outcomes, and in 
+      certain cases, the measurement may not be beneficial to use.
+
+      Gender
+      The development and body fat composition of girls and boys vary with age. Consequently, 
+      a child's age and gender are considered when evaluating their BMI.
+
+      Age
+      In aging individuals, increased body fat and muscle loss may cause BMI to underestimate 
+      body fat content.
+
+      Muscle
+      BMI may misclassify muscular individuals as overweight or obese, as it doesn't 
+      differentiate muscle from fat.
+
+      Pregnancy
+      Expectant mothers experience weight gain due to their growing baby. Maintaining a 
+      healthy pre-pregnancy BMI is advisable to minimise health risks for both mother 
+      and child.
+
+      Race
+      Certain health concerns may affect individuals of some Black and Asian origins at 
+      lower BMIs than others. To learn more, it is advised to discuss this with your 
+      GP or practice nurse.
+    </main>
   );
 }
